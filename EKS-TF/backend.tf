7ef6bfd1ea9e2c804fc9d96c1cdb9a63a6020fb1
VINOD-TEC <65216682+VINOD-TEC@users.noreply.github.com>
2026-03-26 11:01:53 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -9,7 +9,7 @@ terraform {
   required_version = ">=1.14.0"
   required_providers {
     aws = {
-      version = ">= 5.49.0"
+      version = ">= 6.23.0"
       source  = "hashicorp/aws"
     }
   }
